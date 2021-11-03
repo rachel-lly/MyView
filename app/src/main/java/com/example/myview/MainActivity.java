33d93ea45ca000cce99706b6e4f1a1ab34e6528d
rachel-lly <3556753750@qq.com>
2021-11-03 22:25:08 +0800

    floatingButton

--- app/src/main/java/com/example/myview/MainActivity.java
+++ app/src/main/java/com/example/myview/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.myview;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.example.myview.BottomDragLayout.BottomDragActivity;
 import com.example.myview.LoadingView.LoadingView;
@@ -15,9 +26,13 @@ import com.example.myview.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private static final int GET_DIALOG_PERMISSION = 111;
+    private static String TAG = "MainActivity";
+
     private ActivityMainBinding mainBinding;
     private LoadingView loadingView;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +42,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setContentView(mainBinding.getRoot());
 
+        try {
+            showButton();
+        } catch (Exception e) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, GET_DIALOG_PERMISSION);
+        }
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_DIALOG_PERMISSION) {
+            showButton();
+        }
+    }
+
+    private void showButton(){
+        WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Button button = new Button(MainActivity.this);
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,0,0, PixelFormat.TRANSPARENT);
+
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+        layoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        layoutParams.gravity = Gravity.START | Gravity.TOP;
+        layoutParams.x = 100;
+        layoutParams.y = 100;
+        button.setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        initialX = layoutParams.x;
+                        initialY = layoutParams.y;
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        layoutParams.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        layoutParams.y = initialY + (int) (event.getRawY() - initialTouchY);
+                        windowManager.updateViewLayout(button, layoutParams);
+                        return true;
+                }
+                return false;
+            }
+        });
+        windowManager.addView(button,layoutParams);
     }
 
     private void setClick() {
